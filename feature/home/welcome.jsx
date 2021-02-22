78fdd1755b62340ecfbfa6571eaf9de9ff678895
Dissmind <dissmind@gmail.com>
2021-02-22 17:29:31 +0200

    init

---
+++ feature/home/welcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const WelcomeStl = styled.div``
+
+export const Welcome = ({}) => {
+  return (
+    <WelcomeStl>
+      <h1>Welcome to NEXT.js site</h1>
+    </WelcomeStl>
+  )
+}
